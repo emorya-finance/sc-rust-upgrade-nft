0d9fc0ca3c7c076411ae761f6e29d6d0b24c147c
Toderean <todereanrazvan@yahoo.com>
2024-10-03 16:23:03 +0300

    resetNft endpoint

--- src/nftupgrade.rs
+++ src/nftupgrade.rs
@@ -10,6 +10,10 @@ struct NftAttributes {
 pub trait NftUpgradeModule:
     crate::storage::StorageModule + crate::private::PrivateModule + crate::owner::OwnerModule
 {
+    /// upgrade_nft
+    /// receive the NFT,
+    /// read the attributes,
+    /// identify the level and increase it
     #[endpoint(upgradeNft)]
     #[only_owner]
     fn upgrade_nft(&self, user: ManagedAddress) {
@@ -47,10 +51,55 @@ pub trait NftUpgradeModule:
                 &user,
                 &emorya_nft_payment.token_identifier,
                 &BigUint::from(emorya_nft_payment.token_nonce),
-                self.blockchain().get_gas_left(),            
+                self.blockchain().get_gas_left(),
                 &encoded_attributes,
-                &args, 
+                &args,
             )
             .expect("Failed to transfer the updated NFT.");
+    }
+
+    #[endpoint(resetNft)]
+    #[only_owner]
+    fn reset_nft(&self, user: ManagedAddress) {
+        self.require_not_paused();
+
+        let payment = self.call_value().all_esdt_transfers();
+
+        require!(payment.len() == 1, "Only one NFT should be sent!");
+
+        let mut emr_nft_payment = None;
+        let emr_nft = self.emr_nft().get();
+
+        for data in payment.iter() {
+            if data.token_identifier == emr_nft {
+                emr_nft_payment = Some(data);
+            }
+        }
+
+        let emorya_nft_payment = emr_nft_payment.expect("Emorya NFT payment not found.");
+
+        let mut nft_attr = self.blockchain().get_token_attributes::<NftAttributes>(
+            &emorya_nft_payment.token_identifier,
+            emorya_nft_payment.token_nonce,
+        );
+        nft_attr.level -= 1;
+
+        let mut encoded_attributes = ManagedBuffer::new();
+        nft_attr.top_encode(&mut encoded_attributes).unwrap();
+
+        let mut args = ManagedArgBuffer::new();
+        args.push_arg(&user);
+        let _ = self
+            .send_raw()
+            .transfer_esdt_execute(
+                &user,
+                &emorya_nft_payment.token_identifier,
+                &BigUint::from(emorya_nft_payment.token_nonce),
+                self.blockchain().get_gas_left(),
+                &encoded_attributes,
+                &args,
+            )
+            .expect("Failed to transfer the updated NFT.");
+
     }
 }
