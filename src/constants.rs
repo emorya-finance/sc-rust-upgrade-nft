f8a5b325759ba613fea4143994b34e792b5a0757
Kostas Tzoumpas <tzoumpas.ks@gmail.com>
2024-10-15 12:12:08 -0500

    constants example

--- src/constants.rs
+++ src/constants.rs
@@ -3,3 +3,12 @@ pub const IPFS_CID: &[u8] = b".ipfs.nftstorage.link";
 pub const TAGS: &[u8] = b"Emorya,Sports,NFT";
 
 pub const NFT_IDENTIFIER: &[u8] = b"EMRNFT-";
+
+// EXAMPLE FOR EMR NFT
+
+// pub const IPFS_CID: &[u8] =
+// b"bafybeihggf4ao72jrpc7oeyvyrfm6srxbtwybiyans4e37pxxxgrhrjx4y.ipfs.nftstorage.link";
+
+// pub const TAGS: &[u8] = b"Emorya,Sports,NFT";
+
+// pub const NFT_IDENTIFIER : &[u8] = b"EMRNFT-041abf";
