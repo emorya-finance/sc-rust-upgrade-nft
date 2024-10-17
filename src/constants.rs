57569f16dd77b0b85beb3541865a8516009204fd
Toderean <todereanrazvan@yahoo.com>
2024-10-17 11:13:42 +0300

    Emorya Investors NFt constans

--- src/constants.rs
+++ src/constants.rs
@@ -2,7 +2,7 @@ pub const IPFS_CID: &[u8] = b".ipfs.nftstorage.link";
 
 pub const TAGS: &[u8] = b"Emorya,Sports,NFT";
 
-pub const NFT_IDENTIFIER: &[u8] = b"EMRNFT-";
+pub const NFT_IDENTIFIER: &[u8] = b"EMRNFT-50e862";
 
 // EXAMPLE FOR EMR NFT
 
