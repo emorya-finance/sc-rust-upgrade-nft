bd5f79086f811fb2cab3e60848b46b29ade9406c
Kostas Tzoumpas <kststz@gmail.com>
2024-11-20 19:45:58 +0200

    fully build

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-cargo fmt && cargo clippy && mxpy contract build && sc-meta all proxy && abi-markdowner --output-file README.md
+# pip install abi-markdowner
+cargo fmt && cargo clippy && sc-meta all build && sc-meta all proxy && abi-markdowner --output-file README.md
