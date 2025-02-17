35e8c50c481aa2e56f1ca0fd01c5b98fad6f71fb
Toderean <todereanrazvan@yahoo.com>
2025-02-17 17:50:50 +0200

    devnet push

--- interaction/run.sh
+++ interaction/run.sh
@@ -8,9 +8,11 @@ source snippets.sh
 ############ START ############
 
 # deploy
-upgrade
+# upgrade
 
 # assignRole
 
 # runTx $OWNER_ADDRESS "" ESDTNFTTransfer@$NFT_HEX@$NONCE@01@$SC_ADDRESS_HEX@$(python3 to_hex.py initialize)
 
+# runTx erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a5w6u "" controlChanges@$(python3 to_hex.py EMR-d10ed9)@$(python3 to_hex.py canFreeze)@$(python3 to_hex.py false)@$(python3 to_hex.py canPause)@$(python3 to_hex.py false)@$(python3 to_hex.py canWipe)@$(python3 to_hex.py false)
+runTx erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a5w6u "" setSpecialRole@$(python3 to_hex.py EMR-d10ed9)@$(mxpy wallet bech32 --decode $OWNER_ADDRESS)@$(python3 to_hex.py ESDTRoleLocalBurn)
--- interaction/view.sh
+++ interaction/view.sh
@@ -7,8 +7,9 @@ fi
 source snippets.sh
 ############ START ############
 
-view getNftAttributesLevelBeforeUpgrade "0x$NFT_HEX 0x$NONCE_HEX"
+# view getNftAttributesLevelBeforeUpgrade "0x$NFT_HEX 0x$NONCE_HEX"
 # view getNftAttributes "0x$OWNER_ADDRESS_HEX 0x$NFT_HEX 0x$NONCE_HEX"
-# view getNftUriJson "0x$OWNER_ADDRESS_HEX 0x$NFT_HEX 0x$NONCE_HEX"
+view getNftLevelByAddress "0x$(mxpy wallet bech32 --decode erd16d8t0fm6gp5x2ute024pamg666frmddhzjp5qhz8ady2mfhe6yrsqqdu9n)"
+view getNftLevelByAddress "0x$(mxpy wallet bech32 --decode erd18l643ls27yp2l0ky9tth596kws9kppdjm4humdxqw3jxcsky0znq8r0frh)"
 
 
